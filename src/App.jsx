844715fc1aacd99eb4ee5c567bdf2c984464619f
Micaela <micaela.valmores@gmail.com>
2025-10-02 20:51:01 -0700

    Create the Hit Ingredients Section and apply to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from "./assets/components/NavBar";
 import TopRankedItem from "./assets/components/TopRankedItem";
 import ForYou from "./assets/components/ForYou";
+import HitIngredients from "./assets/components/HitIngredients";
 
 function App () {
     return (
@@ -13,21 +14,21 @@ function App () {
                     <div className="Top1">
                         <TopRankedItem
                             ranking={1} 
-                            image={<img src="/" alt="Product" />} 
+                            image={<img src="/larocheposay.jpg" width="100" alt="Product" />} 
                             brand="BrandName" 
                             product="ProductName" 
                             rating={4.5}
                         />
                         <TopRankedItem
-                            ranking={1} 
-                            image={<img src="/" alt="Product" />} 
+                            ranking={2} 
+                            image={<img src="/larocheposay.jpg" width="100" alt="Product" />} 
                             brand="BrandName" 
                             product="ProductName" 
                             rating={4.5}
                         />
                         <TopRankedItem
-                            ranking={1} 
-                            image={<img src="/" alt="Product" />} 
+                            ranking={3} 
+                            image={<img src="/larocheposay.jpg" width="100" alt="Product" />} 
                             brand="BrandName" 
                             product="ProductName" 
                             rating={4.5}
@@ -35,22 +36,22 @@ function App () {
                     </div>
                         <div className="Top4">
                         <TopRankedItem
-                                ranking={1} 
-                                image={<img src="/" alt="Product" />} 
+                                ranking={4} 
+                                image={<img src="/larocheposay.jpg" width="100" alt="Product" />} 
                                 brand="BrandName" 
                                 product="ProductName" 
                                 rating={4.5}
                             />
                             <TopRankedItem
-                                ranking={1} 
-                                image={<img src="/" alt="Product" />} 
+                                ranking={5} 
+                                image={<img src="/larocheposay.jpg" width="100" alt="Product" />} 
                                 brand="BrandName" 
                                 product="ProductName" 
                                 rating={4.5}
                             />
                             <TopRankedItem
-                                ranking={1} 
-                                image={<img src="/" alt="Product" />} 
+                                ranking={5} 
+                                image={<img src="/larocheposay.jpg" width="100" alt="Product" />} 
                                 brand="BrandName" 
                                 product="ProductName" 
                                 rating={4.5}
@@ -62,23 +63,41 @@ function App () {
                 <h3>FOR YOU</h3>
                 <div className="ForYouList">
                     <ForYou 
-                        image={<img src="/" alt="Example Image 1" />} 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Example Image 1" />} 
                         description="This is a description for the first card." 
                     />   
                     <ForYou 
-                        image={<img src="/" alt="Example Image 1" />} 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Example Image 1" />} 
                         description="This is a description for the first card." 
                     />   
                     <ForYou 
-                        image={<img src="/" alt="Example Image 1" />} 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Example Image 1" />} 
                         description="This is a description for the first card." 
                     />   
                     <ForYou 
-                        image={<img src="/" alt="Example Image 1" />} 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Example Image 1" />} 
                         description="This is a description for the first card." 
                     />    
                 </div>
             </div>
+            <div className="HitIngredientsScetion">
+                <h3>HIT INGREDIENTS THIS YEAR</h3>
+                <div className="IngredientsList">
+                    <HitIngredients 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Ingredient 1" />} 
+                        name="Ingredient 1"
+                    />
+                    <HitIngredients 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Ingredient 1" />} 
+                        name="Ingredient 1"
+                    />
+                    <HitIngredients 
+                        image={<img src="/larocheposay.jpg" width="100" alt="Ingredient 1" />} 
+                        name="Ingredient 1"
+                    />
+                </div>
+            </div>
+            <footer></footer>
         </div>
     )
 }
