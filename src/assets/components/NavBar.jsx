85c9e38849a8dd1780b8fa2942ecf85a283e4b9b
Micaela <micaela.valmores@gmail.com>
2025-10-02 19:04:08 -0700

    make NavBar component

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,8 +1,13 @@
+import './HomeComponents.css';
+
 function NavBar () {
     return (
-        <div>
-            <img src="/" width="100" alt="GlowrLogo" />
-            <div>
+
+
+        <div className='Header'>
+            <img src="/Glowr.svg" width="100" alt="GlowrLogo" />
+            <div className='SearchBar'>
+                <img src="/search.svg" width="30" alt="SearchIcon" />
                 Search...
             </div>
         </div>
