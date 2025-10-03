5d3afb3b4380c28d9504e711bb340960297ffc49
Micaela <micaela.valmores@gmail.com>
2025-10-02 18:36:57 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import NavBar from "./assets/components/NavBar";
+
+function App () {
+    return (
+        <NavBar />
+    )
+}
+
+export default App;
---
+++ src/assets/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar () {
+    return (
+        <div>
+            <img src="/" width="100" alt="GlowrLogo" />
+            <div>
+                Search...
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
